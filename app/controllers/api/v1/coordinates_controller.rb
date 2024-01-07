4f6f75df9457705776b3f4be4300f6dcaa98e3a3
Ethan Bustamante <ethanjamesb2004@gmail.com>
2024-01-07 16:21:11 -0700

    Transfer service functionality; create facade and controller

---
+++ app/controllers/api/v1/coordinates_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::CoordinatesController < ApplicationController
+  def index
+    address = params[:address]
+
+    facade = CoordinatesFacade.new(address: address)
+    coordinates = facade.coordinates
+
+    if coordinates.present?
+      render json: { coordinates: coordinates }
+    else
+      render json: { error: 'Coordinates not found' }, status: :not_found
+    end
+  end
+end
